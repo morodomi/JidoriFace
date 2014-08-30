6cbe2b8b773d25b38268b91c6f1335c3cb85f41b
Satoshi Yazawa <yazawa@yzwlab.net>
2014-08-30 16:58:03 +0900

    Removed unnecessary codes

--- Wearable/src/main/java/com/example/android/wearable/datalayer/MainActivity.java
+++ Wearable/src/main/java/com/example/android/wearable/datalayer/MainActivity.java
@@ -97,53 +97,6 @@ public class MainActivity extends Activity {
 
     }
 
-    private static class DataItemAdapter extends ArrayAdapter<Event> {
-
-        private final Context mContext;
-
-        public DataItemAdapter(Context context, int unusedResource) {
-            super(context, unusedResource);
-            mContext = context;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            ViewHolder holder;
-            if (convertView == null) {
-                holder = new ViewHolder();
-                LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(
-                        Context.LAYOUT_INFLATER_SERVICE);
-                convertView = inflater.inflate(android.R.layout.two_line_list_item, null);
-                convertView.setTag(holder);
-                holder.text1 = (TextView) convertView.findViewById(android.R.id.text1);
-                holder.text2 = (TextView) convertView.findViewById(android.R.id.text2);
-            } else {
-                holder = (ViewHolder) convertView.getTag();
-            }
-            Event event = getItem(position);
-            holder.text1.setText(event.title);
-            holder.text2.setText(event.text);
-            return convertView;
-        }
-
-        private class ViewHolder {
-
-            TextView text1;
-            TextView text2;
-        }
-    }
-
-    private class Event {
-
-        String title;
-        String text;
-
-        public Event(String title, String text) {
-            this.title = title;
-            this.text = text;
-        }
-    }
-
     IntentFilter localIntentFilter = new IntentFilter("com.google.android.clockwork.home.action.BACKGROUND_ACTION");
     private final BroadcastReceiver mActionReceiver = new BroadcastReceiver() {
         public void onReceive(Context context, Intent data) {
